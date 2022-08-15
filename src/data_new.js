c5a77c0db64250634a1ba262b6ca83dfd91a733d
Zuzeido <79529059+Zuzeido@users.noreply.github.com>
2022-08-15 19:55:51 +0200

    add new items
    add new items

---
+++ src/data_new.js
@@ -0,0 +1,75 @@
+const menu = [
+    /* Entrantes */
+    {
+        id: "1",
+        title: "Fingers de pollo crujientes",
+        category: "Entrantes",
+        price: "9,50",
+        img: "./images/fingers.jpeg",
+        desc: ""
+    },
+    {
+        id: "11",
+        title: "Tabla de ibéricos",
+        category: "Entrantes",
+        price: "12",
+        img: "./images/tabla.jpeg",
+        desc: ""
+    },
+    {
+        id: "12",
+        title: "Tabla de ahumados",
+        category: "Entrantes",
+        price: "13",
+        img: "./images/ahumados.jpeg",
+        desc: ""
+    },
+    {
+        id: "13",
+        title: "Mix de croquetas",
+        category: "Entrantes",
+        price: "12",
+        img: "",
+        desc: ""
+    },
+    {
+        id: "14",
+        title: "Cazuela de huevos rotos",
+        category: "Entrantes",
+        price: "12,50",
+        img: "./images/huevos.jpeg",
+        desc: ""
+    },
+    {
+        id: "15",
+        title: "Costillas asadas a la barbacoa",
+        category: "Entrantes",
+        price: "15",
+        img: "",
+        desc: ""
+    },
+    {
+        id: "16",
+        title: "Brochetas de pollo Yakitori",
+        category: "Entrantes",
+        price: "15",
+        img: "",
+        desc: ""
+    },
+    {
+        id: "17",
+        title: "Envoltino de langostino y patata deleita",
+        category: "Entrantes",
+        price: "15",
+        img: "",
+        desc: ""
+    },
+    {
+        id: "18",
+        title: "Tempura de verduras",
+        category: "Entrantes",
+        price: "12",
+        img: "",
+        desc: ""
+    }
+]
